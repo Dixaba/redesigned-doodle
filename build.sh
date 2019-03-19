1b3c03266a94f637b1a1353a2c2864400c5cef55
Dixaba <dixaba@yandex.ru>
2019-03-20 00:38:14 +0300

    Yaay

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+REPONAME=dixaba/redesigned-doodle
+
+for NAME in latest-big latest latest-x64 latest-both
+do
+  FILENAME=$(sed -e 's/\-/\./g; s/latest\.\?//; s/$/\.Dockerfile/; s/^\.//' <<< $NAME)
+  docker build -f $FILENAME -t $REPONAME:$NAME .
+  VERSION=$(grep -oP 'Using Qt version \K[0-9.]+' <<< $(docker run --rm $REPONAME:$NAME qmake --version))
+  docker image tag $REPONAME:$NAME $REPONAME:$(sed "s/latest/$VERSION/" <<< $NAME)
+done
